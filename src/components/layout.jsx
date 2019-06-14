fa8a2dc865953b0f7a2a60405b9050b8ea66a608
Agillmann <adrien.gillmann@gmail.com>
2019-06-14 14:42:24 +0200

    add particlesjs multi colors

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from "react"
 import Header from "./header"
 import Footer from "./footer"
-
+import Particles from "react-particles-js"
 import '../scss/main.scss';
+
+
+
 
 class Layout extends Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
+  particlesOptions = {
+    
+  }
   render() {
     return (
       <main>
+        <Particles className='particles' params={{
+          "particles": {
+            "number": {
+                "value": 50
+            },
+            "size": {
+                "value": 3
+            },
+            "color": {
+              "value": ["#005AFF","#ffa500"]
+            },
+        },
+        
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                }
+            }
+        }
+        }}/>
         <Header />
         {this.props.children}
         <Footer />
