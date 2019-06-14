aef499a6c21a15f3969148c8b2e94ae3c48c2b5e
Agillmann <adrien.gillmann@gmail.com>
2019-06-14 16:56:37 +0200

    add particles + img

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,42 +4,37 @@ import Footer from "./footer"
 import Particles from "react-particles-js"
 import '../scss/main.scss';
 
-
-
-
 class Layout extends Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
   particlesOptions = {
-    
+    "particles": {
+      "number": {
+          "value": 50
+      },
+      "size": {
+          "value": 3
+      },
+      "color": {
+        "value": ["#005AFF","#ffa500"]
+      },
+    },
+  
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
   }
   render() {
     return (
       <main>
-        <Particles className='particles' params={{
-          "particles": {
-            "number": {
-                "value": 50
-            },
-            "size": {
-                "value": 3
-            },
-            "color": {
-              "value": ["#005AFF","#ffa500"]
-            },
-        },
-        
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                }
-            }
-        }
-        }}/>
+        <Particles className='particles' params={this.particlesOptions}/>
         <Header />
         {this.props.children}
         <Footer />
